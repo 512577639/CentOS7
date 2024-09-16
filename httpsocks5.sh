6414257bb330fe8cf685f0a664a399574bd39e9e
512577639 <52204748+512577639@users.noreply.github.com>
2024-09-16 20:59:50 +0800

    httpsocks5.sh

--- httpsocks5.sh
+++ httpsocks5.sh
@@ -4,9 +4,9 @@ function install_http() {
   yum install -y squid
   yum install -y net-tools
   cat <<EOF >/etc/squid/squid.conf
-  
+  #
   # Recommended minimum configuration:
-  
+  #
   # Example rule allowing access from your local networks.
   # Adapt to list your (internal) IP networks from where browsing
   # should be allowed
@@ -15,7 +15,7 @@ function install_http() {
   acl localnet src 192.168.0.0/16 # RFC1918 possible internal network
   acl localnet src fc00::/7       # RFC 4193 local private network range
   acl localnet src fe80::/10      # RFC 4291 link-local (directly plugged) machines
-  
+  #
   acl SSL_ports port 443
   acl Safe_ports port 80          # http
   acl Safe_ports port 21          # ftp
@@ -28,53 +28,53 @@ function install_http() {
   acl Safe_ports port 591         # filemaker
   acl Safe_ports port 777         # multiling http
   acl CONNECT method CONNECT
-  
+  #
   # Recommended minimum Access Permission configuration:
-  
+  #
   # Deny requests to certain unsafe ports
   http_access deny !Safe_ports
-  
+  #
   # Deny CONNECT to other than secure SSL ports
   http_access deny CONNECT !SSL_ports
-  
+  #
   # Only allow cachemgr access from localhost
   http_access allow localhost manager
   http_access deny manager
-  
+  #
   # We strongly recommend the following be uncommented to protect innocent
   # web applications running on the proxy server who think the only
   # one who can access services on "localhost" is a local user
   #http_access deny to_localhost
-  
+  #
   # INSERT YOUR OWN RULE(S) HERE TO ALLOW ACCESS FROM YOUR CLIENTS
-  
+  #
   # Example rule allowing access from your local networks.
   # Adapt localnet in the ACL section to list your (internal) IP networks
   # from where browsing should be allowed
   http_access allow localnet
   http_access allow localhost
-  
+  #
   acl client src 0.0.0.0/0
   http_access allow client
-  
+  #
   request_header_access X-Forwarded-For deny all
   request_header_access From deny all
   request_header_access Via deny all
-  
+  #
   # And finally deny all other access to this proxy
   http_access deny all
-  
+  #
   # Squid normally listens to port 3128
   http_port 32122
-  
+  #
   # Uncomment and adjust the following to add a disk cache directory.
   # cache_dir ufs /var/spool/squid 100 16 256
-  
+  #
   # Leave coredumps in the first cache dir
   coredump_dir /var/spool/squid
-  
+  #
   # Add any of your own refresh_pattern entries above these.
-  
+  #
   refresh_pattern ^ftp:           1440    20%     10080
   refresh_pattern ^gopher:        1440    0%      1440
   refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
